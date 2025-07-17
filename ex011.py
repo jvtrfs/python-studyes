d035f1166eda2b269bf46f5f74b32840a241d6a8
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-17 19:12:24 -0300

    resposta
    resposta sobre a largura e a area da parede perguntadas no programa

--- ex011.py
+++ ex011.py
@@ -1,3 +1,4 @@
 larg = float(input('Largura da parede'))
 alt = float(input('Altura da parede'))
 area = larg * alt
+print('Sua parede tem a dimensao de {}x{} e a area e de {} '.format(larg, alt, area))
