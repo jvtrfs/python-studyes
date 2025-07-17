1618bcd758f044d898186a367d566d7dd3cf45b4
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-17 19:17:38 -0300

    resposta
    resposta para sabermos a altura, area e a largura da parede

--- ex011.py
+++ ex011.py
@@ -2,6 +2,7 @@ larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
 area = larg * alt
 print('Sua parede tem a dimensao de {}x{} e a area e de {} '.format(larg, alt, area))
+print('Sua parede tem a dimensao de {}x{} e a area e de {}m² '.format(larg, alt, area))
 tinta = area / 2
 print = ('A quantidade de tinta necessaria e {}'.format (tinta))
 
