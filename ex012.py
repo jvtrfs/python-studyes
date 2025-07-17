2631282b4d7b13357a56cf99650a5c6b0353c08f
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-17 19:46:44 -0300

    resposta
    apos informar o preco antigo, a resposta dara o preco antigo e o preco na promocao

--- ex012.py
+++ ex012.py
@@ -1,2 +1,3 @@
 preco = float(input('Qual e o preco do produto? R$'))
 novo = preco - (preco * 5/ 100)
+print('O preco do produto que custava R${:.2f}, na promocao esta custando R${:.2f}.' .format(preco, novo))
