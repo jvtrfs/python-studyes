c6d225735dfbfdb0d4fa24ca47b6176d47ccf173
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-18 20:21:47 -0300

    resposta
    resposta da nossa pergunta, informando o salario antigo e o salario com 15% de aumento do mesmo

--- ex013.py
+++ ex013.py
@@ -1,2 +1,3 @@
 salario = float(input('Qual o salario do funcionario? R$'))
 novo = salario + (salario * 15 / 100)
+print('Um funcionario que ganhava {:.2f}, com um aumento de 15% em seu salario ganhara R${:.2f}'.format(salario, novo))
