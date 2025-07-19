a8f5bc4397aef8b18e3e75ceb14148e3ef1c1137
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-19 19:37:32 -0300

    resposta
    resposta da nossa pergunta e nossa forumula, mostrando as temperaturas em duas unidades de temperaturas diferentes

--- ex0014.py
+++ ex0014.py
@@ -1,3 +1,4 @@
 c = float(input('Informe a temperatura em C:'))
 f = ((9 * c) / 5) + 32
 # nao precisa de parenteses mas e bom colocar para melhor identificacao
+print('A temperatura em {}°C corresponde a {}°F'.format(c, f))
