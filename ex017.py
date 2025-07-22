aff199eca49e49d073fc0e399f7a0132a35a141b
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:50:36 -0300

    importando a hypot diretamente
    importando hypot diretamente para deixar o codigo ainda mais limpo

--- ex017.py
+++ ex017.py
@@ -6,4 +6,5 @@ print('a hipotenusa mede {:.2f}'.format(hi))'''
 
 # podemos fazer importar diretamente a hypot da biblioteca math para facilitar ainda mais, segue abaixo.
 
+from math import hypot
 # importando a hypot em math, o codigo fica mais limpo e bonito
