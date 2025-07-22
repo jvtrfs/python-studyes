08f0f62e98d285f6c6b715905e8cf505e7961a1d
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:23:21 -0300

    comentario
    comentario explicativo para nao perder a linha de raciocinio

--- ex016.py
+++ ex016.py
@@ -6,3 +6,5 @@ print('o valor digitado foi {} e o inteiro dele e {}'.format(num, trunc(num)))''
 
 '''num = float(input('digite um valor: '))
 print('o numero digitado foi {} e o inteiro dele e {}'.format(num, int(num)))'''
+
+# usamos a mesma logica de programacao para fazer a mesma coisa, so que usando uma funcao mais simples, sem importar modulos
