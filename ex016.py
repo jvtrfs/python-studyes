e44f3e08ec34cd09809bf6dd92f7ad32be05ffec
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:23:07 -0300

    programa 2
    utilizamos a mesma logica de programacao porem usando uma funcao simples sem importar nenhuma biblioteca, tambem serve!

--- ex016.py
+++ ex016.py
@@ -4,3 +4,5 @@ print('o valor digitado foi {} e o inteiro dele e {}'.format(num, trunc(num)))''
 
 # outro exemplo de programa sem precisar fazer essa importacao
 
+'''num = float(input('digite um valor: '))
+print('o numero digitado foi {} e o inteiro dele e {}'.format(num, int(num)))'''
