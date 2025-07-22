da8f3aa33d701f1237fb1bc51f5c9a72617891c2
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:22:28 -0300

    comentario
    comentario explicativo para nao perder a linha de raciocinio

--- ex016.py
+++ ex016.py
@@ -1,3 +1,6 @@
 '''from math import trunc
 num = float(input('digite um valor: '))
 print('o valor digitado foi {} e o inteiro dele e {}'.format(num, trunc(num)))'''
+
+# outro exemplo de programa sem precisar fazer essa importacao
+
