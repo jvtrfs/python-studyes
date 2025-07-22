969defaca88aec6b7bebb159983385814f78bad1
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:49:51 -0300

    comentarios
    comentarios para nao perder o raciocinio

--- ex017.py
+++ ex017.py
@@ -4,3 +4,6 @@ co = float(input('qual o tamanho do cateto adjacente: '))
 hi = math.hypot(ca, co)
 print('a hipotenusa mede {:.2f}'.format(hi))'''
 
+# podemos fazer importar diretamente a hypot da biblioteca math para facilitar ainda mais, segue abaixo.
+
+# importando a hypot em math, o codigo fica mais limpo e bonito
