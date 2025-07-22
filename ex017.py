eb23f092b2995878e74e3bc95306e8336d8ed381
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:51:56 -0300

    resposta2
    resposta das nossas perguntas

--- ex017.py
+++ ex017.py
@@ -10,4 +10,6 @@ from math import hypot
 ca = float(input('qual o tamanho do cateto oposto: '))
 co = float(input('qual o tamanho do cateto adjacente: '))
 hi = hypot(ca, co)
+print('a hipotenusa mede {:.2f}'.format(hi))
+
 # importando a hypot em math, o codigo fica mais limpo e bonito
