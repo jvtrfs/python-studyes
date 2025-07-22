4c3b6c16d2ace8d5c3746addf7a7f8f1cd148ac9
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-22 18:49:24 -0300

    resposta1
    resposta as perguntas acima, dando para o usuario o tamanho do programa a hipotenusa

--- ex017.py
+++ ex017.py
@@ -2,3 +2,5 @@
 ca = float(input('qual o tamanho do cateto oposto: '))
 co = float(input('qual o tamanho do cateto adjacente: '))
 hi = math.hypot(ca, co)
+print('a hipotenusa mede {:.2f}'.format(hi))'''
+
