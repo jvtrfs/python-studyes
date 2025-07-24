b3d7cea5e0fb7d06eb576cebb3a7a86e6aef9955
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-23 22:16:46 -0300

    perguntas
    perguntando os nomes de alunos que vao ser sorteados, do n1 ao n4

--- ex019.py
+++ ex019.py
@@ -1,3 +1,7 @@
 import random
+n1 = str(input('digite o nome do primeriro aluno: '))
+n2 = str(input('digite o nome do segundo aluno: '))
+n3 = str(input('digite o nome do terceiro aluno: '))
+n4 = str(input('digite o nome do quarto aluno: '))
 print('o aluno escolhido foi {}'.format(escolhido))
 
