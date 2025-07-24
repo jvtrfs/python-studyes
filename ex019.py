f8ea5152d2422437fb683c18a0aca85cd4887fc8
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-23 22:17:00 -0300

    lista
    lista de alunos que serao sorteados

--- ex019.py
+++ ex019.py
@@ -3,5 +3,6 @@ n1 = str(input('digite o nome do primeriro aluno: '))
 n2 = str(input('digite o nome do segundo aluno: '))
 n3 = str(input('digite o nome do terceiro aluno: '))
 n4 = str(input('digite o nome do quarto aluno: '))
+lista = [n1, n2, n3, n4]
 print('o aluno escolhido foi {}'.format(escolhido))
 
