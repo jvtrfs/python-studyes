27fd18da3a81aec0271bc506ab1fd160cb063258
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-23 22:13:49 -0300

    n2
    perguntando o nome do segundo aluno

--- ex019.py
+++ ex019.py
@@ -1,4 +1,3 @@
-n2 = str(input('digite o nome do segundo aluno: '))
 n3 = str(input('digite o nome do terceiro aluno: '))
 n4 = str(input('digite o nome do quarto aluno: '))
 lista = [n1, n2, n3, n4]
