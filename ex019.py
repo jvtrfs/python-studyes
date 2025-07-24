b719a8f3ac90613d6a131726feb286638b29d01d
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-23 22:17:28 -0300

    escolhido
    formula para escolhermos um aluno, importando o recurso CHOICE da biblioteca RANDOM

--- ex019.py
+++ ex019.py
@@ -4,5 +4,6 @@ n2 = str(input('digite o nome do segundo aluno: '))
 n3 = str(input('digite o nome do terceiro aluno: '))
 n4 = str(input('digite o nome do quarto aluno: '))
 lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
 print('o aluno escolhido foi {}'.format(escolhido))
 
