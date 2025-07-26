fa854eae2ae4b32f4a4722e867ed5a8f839b916a
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-26 16:20:40 -0300

    resposta 3
    conta como 10 letras por causa do espaco, utilizaremos abaixo uma formula melhor

--- ex022.py
+++ ex022.py
@@ -2,3 +2,4 @@ nome = str(input('Digite seu nome completo: ')).strip() #o strip elimina os espa
 print('analisando seu nome...')
 print('seu nome em mauisculas e: {} '.format(nome.upper()))
 print('seu nome em minusculas e: {}'.format(nome.lower()))
+# print('seu nome ao todoo tem: {} letras'.format(len(nome))) # conta como 10 letras por causa do espaco
