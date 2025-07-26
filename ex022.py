be03aeeb6345a5a6041fe39914623413c0eae4bd
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-26 16:20:16 -0300

    resposta 3
    resposta simples, utilizando a funcao LOWER , para respondermos nosso nome em letra minuscula

--- ex022.py
+++ ex022.py
@@ -1,3 +1,4 @@
 nome = str(input('Digite seu nome completo: ')).strip() #o strip elimina os espacos antes e depois
 print('analisando seu nome...')
 print('seu nome em mauisculas e: {} '.format(nome.upper()))
+print('seu nome em minusculas e: {}'.format(nome.lower()))
