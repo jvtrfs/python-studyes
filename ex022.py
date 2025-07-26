c2990b08ba7e53ab14501a7d582211a87cce0213
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-26 16:23:43 -0300

    resposta 5/2
    formato mais detalhado, transformando o nome em lista e pedindo a posicao do primeiro nome (0) e a quantidade (len) de objetos nesse espaco

--- ex022.py
+++ ex022.py
@@ -6,3 +6,4 @@ print('seu nome em minusculas e: {}'.format(nome.lower()))
 print('seu nome ao todoo tem: {} letras'.format(len(nome) - nome.count(' '))) # a funcao nome.count elimina os espacos do meio do nome, assim fica basicamente (nome com espacos) MENOS (nome sem espacos)
 # print('seu primeiro nome tem {} letras'.format(nome.find(' '))) #formula para achar o primeiro espaco, assim dando a nos o primeiro nome
 separa = nome.split()
+print('o seu primeiro nome e {} e tem {} letras'.format(separa [0], len(separa[0]))) #formato mais detalhado, transformando o nome em lista e pedindo a posicao do primeiro nome (0) e a quantidade (len) de objetos nesse espaco.
