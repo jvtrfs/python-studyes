148ee41c33b26b47be3209de6cab651d50404221
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-30 16:55:48 -0300

    formula
    formula para a palavra ser reconhecida INDEPENDENTE do modelo em que ela e escrita, assim nos livrando de erros no programa

--- ex024.py
+++ ex024.py
@@ -1,2 +1,3 @@
 cid = str(input('em que cidade voce mora? '))
 # print(cid[:5] == 'Santo') o programa esta correto mas tem falhas porque segue fielmente a palavra, assim nao aceitando escritas de formas diferentes, ex: SaNto
+print(cid[:5].upper() == 'SANTO')
