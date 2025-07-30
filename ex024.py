ebaefa457f184dd9ef6c1bef35b0ce786cafa7ab
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-30 16:56:07 -0300

    insight
    insight sobre usuarios

--- ex024.py
+++ ex024.py
@@ -1,3 +1,5 @@
 cid = str(input('em que cidade voce mora? '))
 # print(cid[:5] == 'Santo') o programa esta correto mas tem falhas porque segue fielmente a palavra, assim nao aceitando escritas de formas diferentes, ex: SaNto
 print(cid[:5].upper() == 'SANTO')
+
+# como programador sempre temos que pensar se o usuario vai fazer besteira ou nao na hora de digitar!!
