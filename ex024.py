1cd1524fab4cbf31eafd6c5534632a22319fe75c
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-30 16:55:14 -0300

    comentario
    tenho a formula da resposta mas ha incoerencias dado que o programa segue fielmente o modelo da palavra, abaixo corrigimos  isto

--- ex024.py
+++ ex024.py
@@ -1,1 +1,2 @@
 cid = str(input('em que cidade voce mora? '))
+# print(cid[:5] == 'Santo') o programa esta correto mas tem falhas porque segue fielmente a palavra, assim nao aceitando escritas de formas diferentes, ex: SaNto
