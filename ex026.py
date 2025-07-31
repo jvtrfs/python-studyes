736a539fc1114daa6bfc73eb96e5f85351403d2e
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-07-31 20:49:03 -0300

    print2
    neste print, indicamos em qual posicao a primeira letra A aparece na frase, usando a funcao FIND

--- ex026.py
+++ ex026.py
@@ -1,2 +1,3 @@
 frase = str(input('digite seu nome completo: ')).upper().strip()
 print('a letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('a primeira letra A aparece na posicao {} da frase'.format(frase.find('A')+1))
