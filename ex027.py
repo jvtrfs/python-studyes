e9e0ee7ee83b9240621f84edc8233ef3cad1b4f6
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-01 19:42:39 -0300

    meu programa
    forma como pensei e executei o exercicio, fazendo de acordo com o que aprendi ate agora

---
+++ ex027.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+# meu programa
+# nome = str(input('digite seu nome completo: ')).strip()
+# print('ola, seu primeiro nome e {}'.format(nome[:5]))
+# print('seu ultimo nome e {}'.format(nome[-15:]))
