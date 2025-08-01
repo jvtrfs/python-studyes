bb5caa300db84f0f949937d9d125550e3bea3d7d
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-01 19:46:05 -0300

    resposta3
    usamos a funcao len para sabermos quantas posicoes tem o nome, assim evitando erros

--- ex027.py
+++ ex027.py
@@ -3,6 +3,7 @@ n = str(input('Digite seu nome: '))
 nome = n.split()
 print('muito prazer em te conhecer')
 print('seu primeiro nome e {}'.format(nome[0]))
+print('seu ultimo nome e {}'.format(nome[len(nome)-1])) #usamos a funcao len para sabermos quantas posicoes tem o nome, assim evitando erros
 
 
 
