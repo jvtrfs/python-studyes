71367b5619a6c3703ec9f05a961f9bd9c52890f5
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-05 20:04:22 -0300

    resposta
    seguindo a linha acima, temos a resposta, se o nome for joao, responda "ola joao, tudo bem com voce?"

--- condicoes.py
+++ condicoes.py
@@ -2,3 +2,4 @@
 # todo conteudo sobre as condicoes e formulas no youtube/obsidian# ex de condicao simples
  # nome = str(input('qual e seu nome? ')).lower().strip()
  # if nome == 'joao':
+   # print('ola joao, tudo bem com voce?')
