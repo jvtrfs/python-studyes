555bb22c618e3056ffc22d46f5e1b74f692851ee
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-05 20:03:45 -0300

    if(se)
    condicao if ou "se", se o nome for igual a joao...

--- condicoes.py
+++ condicoes.py
@@ -1,3 +1,4 @@
 /// aula sobre condicoes ///
 # todo conteudo sobre as condicoes e formulas no youtube/obsidian# ex de condicao simples
  # nome = str(input('qual e seu nome? ')).lower().strip()
+ # if nome == 'joao':
