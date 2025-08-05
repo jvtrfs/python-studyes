57e9f32b8d971e900256b24a4c9caf1e9d34e2c0
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-05 20:10:12 -0300

    excluidas
    linhas de anotacoes excluidas

--- condicoes.py
+++ condicoes.py
@@ -1,5 +1,3 @@
-/// aula sobre condicoes ///
-# todo conteudo sobre as condicoes e formulas no youtube/obsidian# ex de condicao simples
 # todo conteudo sobre as condicoes e formulas no youtube/obsidian
 
 # ex de condicao simples
