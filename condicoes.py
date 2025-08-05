943db43effbb518b7aead0d75af5a8d8f204c7df
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-05 20:09:27 -0300

    org
    espacos para organizacao do programa

--- condicoes.py
+++ condicoes.py
@@ -1,11 +1,15 @@
 /// aula sobre condicoes ///
 # todo conteudo sobre as condicoes e formulas no youtube/obsidian# ex de condicao simples
+# todo conteudo sobre as condicoes e formulas no youtube/obsidian
+
+# ex de condicao simples
  # nome = str(input('qual e seu nome? ')).lower().strip()
  # if nome == 'joao':
    # print('ola joao, tudo bem com voce?')
 # else:
    # print('oi {}, tudo bem com voce?'.format(nome))
   #  print('muito bom ter voce aqui novamente ')
+
 # exemplo de condicao composta
 n1 = float(input('digite a primeira nota: '))
 n2 = float(input('digite a segunda nota: '))
