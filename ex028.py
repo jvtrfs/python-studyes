002584125685278da6a004f485408ad1dc06dfe5
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-06 12:54:40 -0300

    commits
    comentarios para melhor entendimento do programa

--- ex028.py
+++ ex028.py
@@ -1,10 +1,10 @@
 from random import randint
 from time import sleep
-computador = randint(0, 5)
+computador = randint(0, 5)  # faz o computador pensar
 print('-=-' * 25)
 print('vou pensar em um numero entre 0 e 5. tente adivinhar...')
 print('-=-' * 25)
-jogador = int(input('em qual numero eu pensei? '))
+jogador = int(input('em qual numero eu pensei? ')) # jogador tenta adivinhar    
 print('aguarde um momento...')
 sleep(2)
 if jogador == computador:
