257ead5654a380e4eab870b1d8ee8fd5e1fcd20c
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-06 13:06:53 -0300

    resposta3
    resposta com o valor que o condutor devera pagar de multas

--- ex029.py
+++ ex029.py
@@ -3,3 +3,4 @@ velocidade = float(input('qual a velocidade do carro? '))
 if velocidade > 80:
     print('MULTADO! voce excedeu o limite da via que e de 80Km/h')
     multa = (velocidade - 80) * 7
+    print('voce devera pagar uma multa de R$ {:.2f}'.format(multa))
