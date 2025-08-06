d3ba25f1fb1cf3be9311c9464c272857a7adcf1a
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-06 13:07:29 -0300

    resposta4
    resposta universal onde independente da condicao (maior ou menor que 80) era aparecera no fim do programa

--- ex029.py
+++ ex029.py
@@ -4,3 +4,4 @@ if velocidade > 80:
     print('MULTADO! voce excedeu o limite da via que e de 80Km/h')
     multa = (velocidade - 80) * 7
     print('voce devera pagar uma multa de R$ {:.2f}'.format(multa))
+print('tenha um bom dia e continue dirigindo com seguranca!')
