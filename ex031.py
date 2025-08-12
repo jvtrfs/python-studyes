cadc8bae6701a6e116db119d1da00b3df7e9fc9d
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-12 20:54:21 -0300

    linhas antigas
    corrigindo linhas, com pontuacoes

--- ex031.py
+++ ex031.py
@@ -1,7 +1,5 @@
-distancia = float(input('qual a distancia da sua viagem? '))
-print ('voce esta prestes a comecar uma viagem de {}Km'.format(distancia))
 if distancia <= 200:
-    preço = distancia * 0.50
+    preco = distancia * 0.50
 else:
-    preço = distancia * 0.45
-print('e o preco da sua passagem e de R${:.2f}'.format(preço))
+    preco = distancia * 0.45
+print('e o preco da sua passagem e de R${:.2f}'.format(preco))
