8ef04310314429039ab23d24fa6afe8d6c2bcf7b
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-15 12:20:52 -0300

    if
    formula de calculo para sabermos se o ano e bissexto ou nao, de acordo com as regras

--- ex032.py
+++ ex032.py
@@ -2,3 +2,4 @@ from datetime import date
 ano = int(input('digite o ano que deseja analisar? coloque 0 para analisar o ano atual:'))
 if ano == 0:
     ano = date.today().year
+if ano % 4 == 0 and ano % 100 !=0 or ano % 400 == 0:
