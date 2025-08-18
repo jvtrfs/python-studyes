88cd7c5aa42afd068029d2d98905bb4f740c8621
João Vitor Rufino <jvtrfsss@hotmail.com>
2025-08-18 20:05:53 -0300

    resposta
    resposta com os salarios atualizados de acordo com as formulas do programa acima

--- ex034.py
+++ ex034.py
@@ -3,3 +3,4 @@ if salario <= 1250:
     novo = salario + (salario * 15 / 100)
 else:
     novo = salario + (salario * 10 / 100)
+print('quem ganhava RS{:.2f}, passa a ganhar R${:.2f}'.format(salario, novo))
